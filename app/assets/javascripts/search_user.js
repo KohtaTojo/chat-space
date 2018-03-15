71f7cdb3ab15801c798737ac2060104f136854c7
東條晃多 <tjkota1090@icloud.com>
2018-03-15 17:20:19 +0900

    Modify search_user.js

--- app/assets/javascripts/search_user.js
+++ app/assets/javascripts/search_user.js
@@ -44,7 +44,7 @@ $(document).on('turbolinks:load',function() {
                     <input name='group[user_ids][]' type='hidden' value='${user_id}'>
                       <p class='chat-group-user__name'>${user_name}</p>
                       <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-                    </div>`
+                  </div>`
         return html;
   }
   $(document).on("click", ".user-search-add", function(){
